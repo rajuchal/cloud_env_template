7590208a02f521e33d41ce0e6682a6fc22740f41
rajuchal <69861082+rajuchal@users.noreply.github.com>
2020-08-20 17:43:28 +0530

    update-2

--- hadoopenv-script.sh
+++ hadoopenv-script.sh
@@ -1,7 +1,7 @@
 adminuser_name=$1
-wget https://github.com/rajuchal/cloud_env_template/archive/master.zip
 sudo apt-get install -y update
 sudo apt install -y unzip
-unzip master.zip
-cd cloud_env_template-master/
-sh install.sh $adminuser_name
+ssh-keygen -q -t rsa -P "" -f ~/.ssh/id_rsa <<<y 2>&1 >/dev/null
+cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
+cat ~/.ssh/id_rsa.pub >> ~/.ssh/known_hosts
+cat $adminuser_name >>t.dat
