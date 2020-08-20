5c40dda1a5d3a5de5e6170b65d502ab9a0352734
rajuchal <69861082+rajuchal@users.noreply.github.com>
2020-08-20 10:22:40 +0530

    Extra script added

---
+++ hadoopenv-script.sh
@@ -0,0 +1,6 @@
+wget https://github.com/rajuchal/cloud_env_template/archive/master.zip
+sudo apt-get install -y update
+sudo apt install -y unzip
+unzip master.zip
+cd cloud_env_template-master/
+sh install.sh
