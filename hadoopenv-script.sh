07f27d79ae91fdc7e24daf25a99448cb5afab405
rajuchal <69861082+rajuchal@users.noreply.github.com>
2020-08-20 19:25:32 +0530

    update-5

--- hadoopenv-script.sh
+++ hadoopenv-script.sh
@@ -3,6 +3,6 @@ sudo apt-get install -y update
 sudo apt install -y unzip
 rm -f ~/.ssh/id_rsa
 ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa && cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
-cat $adminuser_name >>t.dat
+echo $adminuser_name >>t.dat
 sudo mkdir -p /app/bigdata
 sudo chown -R $adminuser_name:$adminuser_name /app
