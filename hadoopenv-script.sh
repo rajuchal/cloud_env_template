69be6e3b00f47dfda704a78220efb2a5ad9d4b8e
rajuchal <69861082+rajuchal@users.noreply.github.com>
2020-08-20 18:01:18 +0530

    update-3

--- hadoopenv-script.sh
+++ hadoopenv-script.sh
@@ -1,9 +1,8 @@
 adminuser_name=$1
 sudo apt-get install -y update
 sudo apt install -y unzip
-ssh-keygen -q -t rsa -P "" -f ~/.ssh/id_rsa <<<y 2>&1 >/dev/null
-cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
-cat ~/.ssh/id_rsa.pub >> ~/.ssh/known_hosts
+rm -f ~/.ssh/id_rsa
+ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa && cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
 cat $adminuser_name >>t.dat
 sudo mkdir -p /app/bigdata
 sudo chown -R $adminuser_name:$adminuser_name /app
