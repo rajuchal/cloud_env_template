4102f807074e6ed0185b563b94a64f4f9d4abd47
rajuchal <69861082+rajuchal@users.noreply.github.com>
2020-08-21 18:01:51 +0530

    update-5

--- hadoopenv-script.sh
+++ hadoopenv-script.sh
@@ -10,12 +10,25 @@ cat /home/$adminuser_name/.ssh/id_rsa.pub >> /home/$adminuser_name/.ssh/known_ho
 sudo chown -R $adminuser_name:$adminuser_name /home/$adminuser_name/.ssh
 ssh -o StrictHostKeyChecking=no $adminuser_name@localhost 'sleep 5 && exit'
 ssh -o StrictHostKeyChecking=no $adminuser_name@0.0.0.0 'sleep 5 && exit'
+
+# ====================================================================
+sudo su $adminuser_name
+cd ~
+
+rm -f ~/.ssh/id_rsa
+ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa && cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
+cat ~/.ssh/id_rsa.pub >> ~/.ssh/known_hosts
+chmod 0600 ~/.ssh/authorized_keys
+
+ssh -o StrictHostKeyChecking=no $USER_NAME@localhost 'sleep 5 && exit'
+ssh -o StrictHostKeyChecking=no $USER_NAME@0.0.0.0 'sleep 5 && exit'
+echo "Sample Outpout ">>t5.dat
 ##==========================================================================
 # Creating Installation directory 
 sudo mkdir -p /app/bigdata
 sudo chown -R $adminuser_name:$adminuser_name /app
 
-wget https://github.com/rajuchal/cloud_env_template/archive/master.zip
-unzip master.zip
-cd cloud_env_template-master/
-sh install.sh $adminuser_name
+#wget https://github.com/rajuchal/cloud_env_template/archive/master.zip
+#unzip master.zip
+#cd cloud_env_template-master/
+#sh install.sh $adminuser_name
