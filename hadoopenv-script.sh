c0aae0c31932b83f8d9c6af2a2553962c822fae3
rajuchal <69861082+rajuchal@users.noreply.github.com>
2020-08-23 21:56:47 +0530

    update-23

--- hadoopenv-script.sh
+++ hadoopenv-script.sh
@@ -6,45 +6,31 @@ sudo apt-get install -y unzip
 #--------------------------------------------------
 # Passwordless ssh login for azureuser
 sudo su - $adminuser_name
+cd
+pwd
+echo $USER
+echo $HOME
+echo `pwd`>>t2.dat
+
 cd /home/$adminuser_name
 
+sudo chown -R $adminuser_name:$adminuser_name /home/$adminuser_name/.ssh
 rm -f /home/$adminuser_name/.ssh/id_rsa
-sudo chown -R $adminuser_name:$adminuser_name /home/$adminuser_name/.ssh
-sudo chmod -R 777 /home/$adminuser_name/.ssh
-ssh-keygen -q -t rsa -P "" -f /home/$adminuser_name/.ssh/id_rsa
+ssh-keygen -q -t rsa -N '' -f /home/$adminuser_name/.ssh/id_rsa
 cat /home/$adminuser_name/.ssh/id_rsa.pub >> /home/$adminuser_name/.ssh/authorized_keys
-cat /home/$adminuser_name/.ssh/id_rsa.pub >> /home/$adminuser_name/.ssh/known_hosts
+#cat /home/$adminuser_name/.ssh/id_rsa.pub >> /home/$adminuser_name/.ssh/known_hosts
 sudo chmod 0600 /home/$adminuser_name/.ssh/authorized_keys
-sudo chown -R $adminuser_name:$adminuser_name /home/$adminuser_name/.ssh
+sudo chown -R $adminuser_name:$adminuser_name /home/$adminuser_name
 
 ## ----------------------------------------------
 echo 'sleep 2 && exit'|ssh -o StrictHostKeyChecking=no $adminuser_name@localhost /bin/bash
 echo 'sleep 2 && exit'|ssh -o StrictHostKeyChecking=no $adminuser_name@0.0.0.0 /bin/bash
-echo `pwd`>>t2.dat
-# ====================================================================
-#	Third create a spark user with proper privileges and ssh keys.
+echo `pwd`>>t3.dat
 
-	sudo addgroup spark
-	sudo useradd -m -g spark spark
-	sudo adduser spark sudo
-	sudo chown spark:spark /home/spark
-
-#	Add to sudoers file:
-
-	echo "spark ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers.d/90-cloud-init-users
-	
-	sudo su - spark
-	cd /home/spark
-	rm -f /home/spark/.ssh/id_rsa
-	ssh-keygen -q -t rsa -P "" -f /home/spark/.ssh/id_rsa && cat /home/spark/.ssh/id_rsa.pub >> /home/spark/.ssh/authorized_keys
-	cat /home/spark/.ssh/id_rsa.pub >> /home/spark/.ssh/known_hosts
-
-	#ssh localhost
-  echo `pwd`>>t3.dat
 ##==========================================================================
 # Creating Installation directory 
 sudo mkdir -p /app/bigdata
-sudo chown -R spark:spark /app
+sudo chown -R $adminuser_name:$adminuser_name /app
 
 #wget https://github.com/rajuchal/cloud_env_template/archive/master.zip
 #unzip master.zip
