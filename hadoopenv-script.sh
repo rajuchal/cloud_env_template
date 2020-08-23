63272d6c12724224a27daa8481cfa3d77cbfa334
rajuchal <69861082+rajuchal@users.noreply.github.com>
2020-08-23 20:05:54 +0530

    update-22

--- hadoopenv-script.sh
+++ hadoopenv-script.sh
@@ -1,25 +1,25 @@
+export DEBIAN_FRONTEND=noninteractive
 adminuser_name=$1
-sudo apt-get install -y update
-sudo apt install -y unzip
+sudo apt-get install -y update 2>/dev/null
+sudo apt-get install -y unzip
 ##==========================================================================
-# Passwordless ssh login for root
-
-rm -f ~/.ssh/id_rsa
-ssh-keygen -q -t rsa -P "" -f ~/.ssh/id_rsa
-cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
-cat ~/.ssh/id_rsa.pub >> ~/.ssh/known_hosts
-echo `pwd`>>t1.dat
 #--------------------------------------------------
 # Passwordless ssh login for azureuser
 sudo su - $adminuser_name
 cd /home/$adminuser_name
+
 rm -f /home/$adminuser_name/.ssh/id_rsa
+sudo chown -R $adminuser_name:$adminuser_name /home/$adminuser_name/.ssh
+sudo chmod -R 777 /home/$adminuser_name/.ssh
 ssh-keygen -q -t rsa -P "" -f /home/$adminuser_name/.ssh/id_rsa
 cat /home/$adminuser_name/.ssh/id_rsa.pub >> /home/$adminuser_name/.ssh/authorized_keys
 cat /home/$adminuser_name/.ssh/id_rsa.pub >> /home/$adminuser_name/.ssh/known_hosts
+sudo chmod 0600 /home/$adminuser_name/.ssh/authorized_keys
 sudo chown -R $adminuser_name:$adminuser_name /home/$adminuser_name/.ssh
-echo 'sleep 2 && exit'|ssh -o StrictHostKeyChecking=no $adminuser_name@localhost
-echo 'sleep 2 && exit'|ssh -o StrictHostKeyChecking=no $adminuser_name@0.0.0.0
+
+## ----------------------------------------------
+echo 'sleep 2 && exit'|ssh -o StrictHostKeyChecking=no $adminuser_name@localhost /bin/bash
+echo 'sleep 2 && exit'|ssh -o StrictHostKeyChecking=no $adminuser_name@0.0.0.0 /bin/bash
 echo `pwd`>>t2.dat
 # ====================================================================
 #	Third create a spark user with proper privileges and ssh keys.
@@ -27,7 +27,6 @@ echo `pwd`>>t2.dat
 	sudo addgroup spark
 	sudo useradd -m -g spark spark
 	sudo adduser spark sudo
-	sudo mkdir /home/spark
 	sudo chown spark:spark /home/spark
 
 #	Add to sudoers file:
@@ -35,10 +34,10 @@ echo `pwd`>>t2.dat
 	echo "spark ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers.d/90-cloud-init-users
 	
 	sudo su - spark
-	cd ~
-	rm -f ~/.ssh/id_rsa
-	ssh-keygen -q -t rsa -P "" -f ~/.ssh/id_rsa && cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
-	cat ~/.ssh/id_rsa.pub >> ~/.ssh/known_hosts
+	cd /home/spark
+	rm -f /home/spark/.ssh/id_rsa
+	ssh-keygen -q -t rsa -P "" -f /home/spark/.ssh/id_rsa && cat /home/spark/.ssh/id_rsa.pub >> /home/spark/.ssh/authorized_keys
+	cat /home/spark/.ssh/id_rsa.pub >> /home/spark/.ssh/known_hosts
 
 	#ssh localhost
   echo `pwd`>>t3.dat
@@ -51,3 +50,7 @@ sudo chown -R spark:spark /app
 #unzip master.zip
 #cd cloud_env_template-master/
 #sh install.sh $adminuser_name
+
+
+#ssh -o StrictHostKeyChecking=no -T $adminuser_name@localhost 'sleep 2 && exit'
+#ssh -o StrictHostKeyChecking=no -T $adminuser_name@0.0.0.0 'sleep 2 && exit'
