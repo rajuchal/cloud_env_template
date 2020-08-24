7a29fee2c92500977b0d2f1a146c663b688385bd
rajuchal <69861082+rajuchal@users.noreply.github.com>
2020-08-24 10:00:09 +0530

    update-26

--- hadoopenv-script.sh
+++ hadoopenv-script.sh
@@ -13,7 +13,7 @@ echo `pwd`>>t4.dat
 rm -f /root/.ssh/id_rsa
 ssh-keygen -q -t rsa -N '' -f /root/.ssh/id_rsa
 cat /root/.ssh/id_rsa.pub >> /root/.ssh/authorized_keys
-chmod 0600 /home/$adminuser_name/.ssh/authorized_keys
+chmod 0600 /root/.ssh/authorized_keys
 ## ----------------------------------------------
 echo 'sleep 2 && exit'|ssh -o StrictHostKeyChecking=no root@localhost /bin/bash
 echo 'sleep 2 && exit'|ssh -o StrictHostKeyChecking=no root@0.0.0.0 /bin/bash
@@ -55,10 +55,11 @@ echo `pwd`>>t5.dat
 	cd /home/spark
 	rm -f /home/spark/.ssh/id_rsa
 	ssh-keygen -q -t rsa -N '' -f /home/spark/.ssh/id_rsa && cat /home/spark/.ssh/id_rsa.pub >> /home/spark/.ssh/authorized_keys
-  
-  echo "Creating Password less login for spark user "
+  	chmod 0600 /home/spark/.ssh/authorized_keys
+  	echo "Creating Password less login for spark user "
 	ssh -o StrictHostKeyChecking=no -T spark@localhost 'sleep 2 && exit'
 	ssh -o StrictHostKeyChecking=no -T spark@0.0.0.0 'sleep 2 && exit'
+	echo `pwd`>>t6.dat
   
 # Creating Installation directory 
 sudo mkdir -p /app/bigdata
