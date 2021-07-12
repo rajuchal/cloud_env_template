6878203d349ba827e1ca19b6767547c19d1af708
Raju Chal <69861082+rajuchal@users.noreply.github.com>
2021-07-12 09:51:26 +0530

    Update hive-env.sh
    12-july-2021

--- hive-env.sh
+++ hive-env.sh
@@ -33,7 +33,7 @@
 #   fi
 # fi
 # The heap size of the jvm stared by hive shell script can be controlled via:
-#
+# ----------------------------------------------------------------------------
 # export HADOOP_HEAPSIZE=1024
 #
 # Larger heap size may be required when running queries over large number of files or partitions. 
